925a712fc31fa9ca7fcc1e51995a204a1a1f4fe8
Rasmus Sjostrom <ras.sjostrom@hotmail.com>
2016-05-23 21:38:23 +0200

    Update readme an setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 
 setup(
     name='hocapontas',
-    version='0.1',
+    version='0.0.1',
     author='Rasmus Sjostrom',
     author_email='ras.sjostrom@hotmail.com',
     packages=[
         'hocapontas', 'hocapontas.scheduler',
-        'hocapontas.storage', 'hocapontas.test'
+        'hocapontas.storage'
     ],
     url='http://pypi.python.org/pypi/hocapontas/',
     license='GNU GPLv3',
-    description='A simple JSON task item scheduler and manager',
+    description='A simple life-assistant API',
     long_description=open('README.rst').read(),
 )
